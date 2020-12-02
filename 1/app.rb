4baac7ea6db23b66f46b7da377f94a9278b0e6f3
Shane Sherman <shane.sherman@gmail.com>
2020-12-01 20:34:53 -0600

    remove unneeded code

--- 1/app.rb
+++ 1/app.rb
@@ -1,6 +1,5 @@
 numbers = File.read('input.txt').split(/\s/).map { |d| d.to_i }
 numbers.each do |number|
-  next_number = numbers.select { |potential_num| number + num < 2020 }
   needed = 2020 - number
   puts "needed: #{needed}"
   found = numbers.detect { |n| n == needed }
