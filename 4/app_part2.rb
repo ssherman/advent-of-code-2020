2133c9d53b7f1531d09feb16c6816727441e2493
Shane Sherman <shane.sherman@gmail.com>
2020-12-04 22:26:23 -0600

    got part 2 working

--- 4/app_part2.rb
+++ 4/app_part2.rb
@@ -11,16 +11,16 @@ def passport_valid?(passport, requirements)
   requirements.each do |req|
     case req
     when :byr
-      return false unless /[0-9]{4}$/.match? passport.data[:byr]
+      return false unless /^[0-9]{4}$/.match? passport.data[:byr]
       return false unless passport.data[:byr].to_i >= 1920 && passport.data[:byr].to_i <= 2002
     when :iyr
-      return false unless /[0-9]{4}$/.match? passport.data[:iyr]
+      return false unless /^[0-9]{4}$/.match? passport.data[:iyr]
       return false unless passport.data[:iyr].to_i >= 2010 && passport.data[:iyr].to_i <= 2020
     when :eyr
-      return false unless /[0-9]{4}$/.match? passport.data[:eyr]
+      return false unless /^[0-9]{4}$/.match? passport.data[:eyr]
       return false unless passport.data[:eyr].to_i >= 2020 && passport.data[:eyr].to_i <= 2030
     when :hgt
-      match_data = /([0-9]+)(cm|in)/.match(passport.data[:hgt])
+      match_data = /^([0-9]+)(cm|in)/.match(passport.data[:hgt])
       return false if match_data.nil?
       number = match_data[1].to_i
       unit = match_data[2]
@@ -30,11 +30,11 @@ def passport_valid?(passport, requirements)
         return false unless number >= 59 && number <= 76
       end
     when :hcl
-      return false unless /\#[0-9a-f]{6}$/.match? passport.data[:hcl]
+      return false unless /^\#[0-9a-f]{6}$/.match? passport.data[:hcl]
     when :ecl
       return false unless ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].include?(passport.data[:ecl])
     when :pid
-      return false unless /[0-9]{9}$/.match? passport.data[:pid]
+      return false unless /^[0-9]{9}$/.match? passport.data[:pid]
     else
       return false
     end
