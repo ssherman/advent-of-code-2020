00a5de89650a035d1fff4da194d9323a5e9c4cfc
Shane Sherman <shane.sherman@gmail.com>
2020-12-06 11:30:11 -0600

    remove unused class

--- 6/app.rb
+++ 6/app.rb
@@ -1,22 +1,13 @@
-class Question
-  attr_accessor :yes
-end
-
-
 data = File.read('input.txt').split(/\s\s/).map do |d|
   lines = d.split(/\s/)
   questions = lines.map do |line|
-    puts "line: #{line.inspect}"
     line.chars.uniq
   end.flatten
-  puts "question: #{questions.uniq}"
   questions.uniq
 end
-#puts data.inspect
 
 count = 0
 data.each do |group|
-  #puts "handling #{group.join} which is adding #{group.length}"
   count += group.length
 end
 puts "answer is #{count}"
